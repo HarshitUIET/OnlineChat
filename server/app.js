bc63dcee5420727f796ef295031c7429f05b2579
Harshit <harshitpachauri76@gmail.com>
2024-07-01 11:08:14 +0530

    Socket completed

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import express from "express";
 import { errorMidddleware } from "./middlewares/error.js";
 import { dbConnect } from "./utilis/features.js";
+import { Server } from "socket.io";
+import {createServer} from "http";
+import {v4 as uuid} from "uuid";
 
 
 import chatRoute from "./routes/chat.js";
@@ -10,14 +13,22 @@ import userRoute from "./routes/user.js";
 import adminRoute from './routes/admin.js'
 
 import { createMessagesInAChat } from "./seeders/chat.js";
+import { NEW_MESSAGE, NEW_MESSAGE_ALERT } from "./constants/event.js";
+import { create } from "domain";
+import { getSockets } from "./lib/helper.js";
+import { Message } from "./models/message.js";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server,{});
 
 dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 3000;
 const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
+
+const userSocketIDs = new Map();
 
 dbConnect(MONGO_URL);
 
@@ -42,10 +53,69 @@ app.get("/",(req,res)=>{
   res.send("Home Page");
 })
 
-app.listen(PORT,()=>{
+io.use((socket,next)=>{})
+
+io.on("connection",(socket) => {
+
+  const user = {
+    _id : "anasn",
+    name : "anas",
+  }
+
+  userSocketIDs.set(user._id.toString(),socket.id);
+  
+  console.log(" a user connected ",socket.id);
+
+  console.log(userSocketIDs);
+
+  socket.on(NEW_MESSAGE,async ({chatId,members,message}) => {
+     const messageForRealTime = {
+       content : message,
+       _id : uuid(),
+        sender : {
+          _id : user._id,
+          name : user.name
+        },
+        chat : chatId,
+        createdAt : new Date().toISOString()
+   }
+
+    const messageForDB = {
+      content : message,
+      sender : user._id,
+      chat : chatId,
+    }
+
+       const membersSocket = getSockets(members);
+
+       io.to(membersSocket).emit(NEW_MESSAGE,{
+          message : messageForRealTime,
+          chatId
+       })
+
+       io.to(membersSocket).emit(NEW_MESSAGE_ALERT,{chatId});
+
+       try {
+        await Message.create(messageForDB);
+       } catch (error) {
+          console.log(error);
+       }
+
+       console.log("New Message",messageForRealTime);
+  })
+
+  socket.on("disconnect",() => {
+    console.log("user disconnected");
+    userSocketIDs.delete(user._id.toString());
+  })
+
+})
+
+server.listen(PORT,()=>{
     console.log(`listening to port no ${PORT} in ${envMode} mode`);
 })
 
 export {
-  envMode
+  envMode,
+  userSocketIDs
 }
